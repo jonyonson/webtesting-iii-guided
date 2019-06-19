b0deb7e161b47eaa8b802e93917e657506c0f695
Jonathan Taylor <jonyonson@icloud.com>
2019-06-19 13:09:28 -0500

    ✅

---
+++ src/helpers.test.js
@@ -0,0 +1,31 @@
+import { savePerson, save } from './helpers.js';
+
+// local to this test file
+jest.mock('uuid', () => {
+  return () => 'abc234';
+});
+
+describe('helpers', () => {
+  it('should return newly saved person', () => {
+    const name = 'brandon';
+    const expected = { id: 'abc234', name };
+
+    const actual = savePerson('brandon');
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('spies', function() {
+  it('reports usage', () => {
+    const bond = jest.fn();
+
+    save('charles', bond);
+
+    expect(bond).toHaveBeenCalled();
+    expect(bond).toHaveBeenCalledTimes(2);
+    expect(bond).toHaveBeenCalledWith('charles');
+    expect(bond).toHaveBeenNthCalledWith(1, 'charles');
+    expect(bond).toHaveBeenNthCalledWith(2, 'charles, james');
+  });
+});
